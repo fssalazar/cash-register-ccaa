9d7f2eb155e7a0867f652756eac2539c792d2776
fssalazar <salazarfelipe.fs@gmail.com>
2024-12-12 10:50:00 -0300

    feat: decimals

--- src/app/components/Layout.tsx
+++ src/app/components/Layout.tsx
@@ -1,14 +1,25 @@
 "use client";
 
-import { Layout } from "antd";
+import { Layout, Button } from "antd";
 import Image from "next/image";
+import Link from "next/link";
+import { signOut } from "next-auth/react";
 import logo from "../../../public/ccaa.png";
 
 export function LayoutCCAA({ children }: { children: React.ReactNode }) {
+  const handleLogout = () => {
+    signOut();
+  };
+
   return (
     <Layout className="h-screen w-screen">
-      <Layout.Header className="bg-blue-900 flex items-center">
-        <Image src={logo.src} alt="Logo" width={60} height={60} priority />
+      <Layout.Header className="bg-blue-900 flex items-center justify-between">
+        <Link href="/">
+          <Image src={logo.src} alt="Logo" width={60} height={60} priority />
+        </Link>
+        <Button type="primary" onClick={handleLogout}>
+          Sair
+        </Button>
       </Layout.Header>
       <Layout.Content className="w-full">{children}</Layout.Content>
     </Layout>
